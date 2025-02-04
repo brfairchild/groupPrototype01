e4acb82a95c2e8535cbf72c2a37f199b8d7efdcd
Eyeo <tsrobertson2@bsu.edu>
2025-02-04 05:43:47 -0500

    Finished House
    Finished house and started to add doors to dev map with intractability

--- project/devMap/player.gd
+++ project/devMap/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
-class_name Player
-@onready var Light1 = $Light1
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.008
@@ -32,25 +31,7 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	if Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue1 == false:
-		print("found you")
-		switch_light1_on()
-	elif Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue1 == true:
-		print("found you")
-		switch_light1_off()
-	if Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue2 == false:
-		print("found you")
-		switch_light2_on()
-	elif Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue2 == true:
-		print("found you")
-		switch_light2_off()
-	if Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue3 == false:
-		print("found you")
-		switch_light3_on()
-	elif Input.is_action_just_pressed("interact") and $RayCast3D.is_colliding() and Global.LightValue3 == true:
-		print("found you")
-		switch_light3_off()
+
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -75,32 +56,3 @@ func toggle_teleport() -> void:
 		position.y += 5  # Move up
 
 	is_teleported_up = !is_teleported_up  # Toggle state
-
-func switch_light1_on() -> void:
-	Global.LightValue1 = true
-	Global.power += 1
-	print(Global.power)
-func switch_light1_off() -> void:
-	Global.LightValue1 = false
-	Global.power += 1
-	print(Global.power)
-func switch_light2_on() -> void:
-	Global.LightValue2 = true
-	Global.power += 1
-	print(Global.power)
-func switch_light2_off() -> void:
-	Global.LightValue2 = false
-	Global.power += 1
-func switch_light3_on() -> void:
-	Global.LightValue3 = true
-	Global.power += 1
-	print(Global.power)
-func switch_light3_off() -> void:
-	Global.LightValue3 = false
-	Global.power += 1
-
-func power_off() -> void:
-	if Global.power == 5:
-		switch_light1_off()
-		switch_light2_off()
-		switch_light3_off()
